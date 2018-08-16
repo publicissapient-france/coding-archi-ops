8fcc5a5064f9807485cbb9d9332e942d80445915
E0370466 <brice.bousquet@sanofi.com>
2018-08-16 17:11:10 +0200

    Modif pour récup des infos

--- ca-ops-referentiel/src/main/java/fr/xebia/ca/ops/referentiel/ReferentielMethods.java
+++ ca-ops-referentiel/src/main/java/fr/xebia/ca/ops/referentiel/ReferentielMethods.java
@@ -1,17 +1,15 @@
 package fr.xebia.ca.ops.referentiel;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.List;
-
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.auth.oauth2.ServiceAccountCredentials;
-import com.google.cloud.storage.Blob;
-import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.StorageOptions;
 
 public class ReferentielMethods {
 
@@ -31,27 +29,18 @@ public class ReferentielMethods {
         return System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
     }
 
+    public static String readResource(final String fileName, Charset charset) throws IOException {
+        return Resources.toString(Resources.getResource(fileName), charset);
+    }
+
     public static String getTrainsFile() {
         return System.getenv("TRAINS_REF_FILE_NAME");
     }
 
     public static List<Train> getTrains() throws IOException {
-/*        GoogleCredentials credentials;
-        File credentialsPath = new File(getCredentialsPath());
-        try (FileInputStream serviceAccountStream = new FileInputStream(credentialsPath)) {
-            credentials = ServiceAccountCredentials.fromStream(serviceAccountStream);
-        }
-*/
-        Storage storage = StorageOptions.newBuilder()
-                .setProjectId(getProjectId())
-//                .setCredentials(credentials)
-                .build()
-                .getService();
-
-        Blob blob = storage.get(getProjectBucket(), getRefPath(getTrainsFile()));
 
         ObjectMapper mapper = new ObjectMapper();
 
-        return mapper.readValue(new String(blob.getContent()), new TypeReference<List<Train>>(){});
+        return mapper.readValue(readResource(getTrainsFile(), Charsets.UTF_8), new TypeReference<List<Train>>(){});
     }
 }
